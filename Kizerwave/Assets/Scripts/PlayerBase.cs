99aa58b1fab3c14faca2d978fb2b366023cccc5c
TheOneJayce <47903533+TheOneJayce@users.noreply.github.com>
2021-01-10 11:22:21 -0500

    PlayerPush

---
+++ Kizerwave/Assets/Scripts/PlayerBase.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBase : ShipBase
+{
+
+    public float fireRate = 5.0f;
+    public bool canFire = true;
+    public PoolManager bulletPool = null;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator DelayedFire()
+    {
+        float timer = fireRate;
+        while (true)
+        {
+            if (canFire == false)
+            {
+                break;
+            }
+
+            if (timer > 0)
+            {
+                timer -= 1 * Time.deltaTime;
+                yield return null;
+            }
+            else
+            {
+                Fire();
+                timer = fireRate;
+            }
+        }
+    }
+    public override void Fire()
+    {
+        if (bulletPool != null)
+        {
+            ProjectileBase defaultProjectile = bulletPool.GetProjectile(this);
+
+        }
+    }
+}
---
+++ Kizerwave/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : PlayerBase
+{
+    public CharacterController characterController;
+    public GameObject player;
+
+
+
+    //Player Movement Verb
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float health;
+    [SerializeField] private float damage;
+    [SerializeField] private float gravityScale;
+
+    public bool playerIsAlive;
+    public bool playerIsPlayable;
+    private Vector3 moveDirection = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        playerIsAlive = true;
+        playerIsPlayable = true;
+        moveSpeed = 20;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float yStore = moveDirection.y;
+        moveDirection = (transform.right * Input.GetAxisRaw("Horizontal")) + (transform.forward * Input.GetAxisRaw("Vertical"));
+        moveDirection = moveDirection.normalized * moveSpeed;
+        moveDirection.y = yStore;
+
+        if (characterController.isGrounded)
+        {
+            moveDirection.y = Physics.gravity.y * Time.deltaTime;
+
+        }
+        else if (!characterController.isGrounded)
+        {
+            moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
+        }
+
+        if (playerIsPlayable)
+        {
+            characterController.Move(moveDirection * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            Fire();
+        }
+
+    }
+}
