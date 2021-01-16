48e93e6265fdc0b994f5eef39d7262c9bb4ff1d0
TheOneJayce <47903533+TheOneJayce@users.noreply.github.com>
2021-01-16 18:49:06 -0500

    PowerUps

---
+++ Kizerwave/Assets/Scripts/PowerUp.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public int powerUpType;
+
+    void OnTriggerEnter(Collider theObject)
+    {
+        if (theObject.CompareTag("Player"))
+        {
+            Pickup(theObject);
+        }
+    }
+
+    void Pickup(Collider theplayer)
+    {
+
+        PlayerScript player = theplayer.GetComponent<PlayerScript>();
+        if (powerUpType == 1)
+        {
+            player.WEAPON = TJayEnums.MissileType.Spread;
+        }
+        else if (powerUpType == 2)
+        {
+            player.WEAPON = TJayEnums.MissileType.Cluster;
+
+        }
+        else if (powerUpType == 3)
+        {
+            player.WEAPON = TJayEnums.MissileType.Protective;
+
+        }
+        else if (powerUpType == 4)
+        {
+            player.WEAPON = TJayEnums.MissileType.Homing;
+
+        }
+        Destroy(gameObject);
+    }
+
+}
